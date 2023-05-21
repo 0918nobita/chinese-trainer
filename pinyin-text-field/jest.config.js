d317403972cfd2c876e96f2d785c5cd66e7ca349
0918nobita <nobita.0918@gmail.com>
2023-05-21 14:24:10 +0900

    ts-jest から vitest に移行した

--- pinyin-text-field/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.m?[jt]s$': ['ts-jest', {
-      tsconfig: '<rootDir>/tsconfig.test.json',
-    }],
-  },
-};
--- pinyin-text-field/src/option.spec.ts
+++ pinyin-text-field/src/option.spec.ts
@@ -1,43 +1,43 @@
+import { describe, expect, it } from 'vitest';
+
 import { Option } from './option';
 
-describe('Option', () => {
-  describe('eq', () => {
-    test('when both are None', () => {
-      const a = Option.none();
-      const b = Option.none();
-      expect(a.eq(b)).toBe(true);
-    });
-
-    test('when lhs is None and rhs is Some(v)', () => {
-      const a = Option.none<number>();
-      const b = Option.some(1);
-      expect(a.eq(b)).toBe(false);
-    });
-
-    test('when lhs is Some(v) and rhs is None', () => {
-      const a = Option.some(1);
-      const b = Option.none<number>();
-      expect(a.eq(b)).toBe(false);
-    });
-
-    test('when inner values are different', () => {
-      const a = Option.some(1);
-      const b = Option.some(2);
-      expect(a.eq(b)).toBe(false);
-    });
-
-    test('when inner values are the same', () => {
-      const a = Option.some(1);
-      const b = Option.some(1);
-      expect(a.eq(b)).toBe(true);
-    });
+describe('eq', () => {
+  it('when both are None', () => {
+    const a = Option.none();
+    const b = Option.none();
+    expect(a.eq(b)).toBe(true);
   });
 
-  test('map', () => {
-    expect(
-      Option.none<number>()
-        .map((x) => x + 1)
-        .eq(Option.none())
-    ).toBe(true);
+  it('when lhs is None and rhs is Some(v)', () => {
+    const a = Option.none<number>();
+    const b = Option.some(1);
+    expect(a.eq(b)).toBe(false);
   });
+
+  it('when lhs is Some(v) and rhs is None', () => {
+    const a = Option.some(1);
+    const b = Option.none<number>();
+    expect(a.eq(b)).toBe(false);
+  });
+
+  it('when inner values are different', () => {
+    const a = Option.some(1);
+    const b = Option.some(2);
+    expect(a.eq(b)).toBe(false);
+  });
+
+  it('when inner values are the same', () => {
+    const a = Option.some(1);
+    const b = Option.some(1);
+    expect(a.eq(b)).toBe(true);
+  });
+});
+
+it('map', () => {
+  expect(
+    Option.none<number>()
+      .map((x) => x + 1)
+      .eq(Option.none())
+  ).toBe(true);
 });
