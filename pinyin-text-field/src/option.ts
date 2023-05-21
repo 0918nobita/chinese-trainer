6ad232fdba5d08048394772df56842f117cc6fa1
0918nobita <nobita.0918@gmail.com>
2023-05-21 14:40:52 +0900

    :zap: Option.none で常に同じインスタンスを返すようにした

--- pinyin-text-field/src/option.ts
+++ pinyin-text-field/src/option.ts
@@ -1,13 +1,15 @@
 export class Option<T> {
   private constructor(private inner: T | null) {}
 
+  static #none = new Option<unknown>(null);
+
   public eq(other: Option<T>): boolean {
     return this.inner === other.inner;
   }
 
   public map<U>(mapping: (inner: T) => U): Option<U> {
-    if (this.inner === null) return this as unknown as Option<U>;
-    return new Option<U>(mapping(this.inner));
+    if (this.inner === null) return Option.#none as Option<U>;
+    return Option.some(mapping(this.inner));
   }
 
   static some<A>(value: A): Option<A> {
@@ -15,12 +17,12 @@ export class Option<T> {
   }
 
   static none<A>(): Option<A> {
-    return new Option<A>(null);
+    return Option.#none as Option<A>;
   }
 
   static fromNullable<A>(nullableValue: A | undefined | null): Option<A> {
     if (nullableValue === undefined || nullableValue === null)
-      return Option.none();
+      return Option.#none as Option<A>;
     return Option.some(nullableValue);
   }
 }
