f33159609b10275693cc6590e3f7f05355ed62fe
0918nobita <nobita.0918@gmail.com>
2023-05-25 12:42:03 +0900

    Rename `Option<T>.bind` to `andThen`

--- pinyin-text-field/src/option.ts
+++ pinyin-text-field/src/option.ts
@@ -1,9 +1,11 @@
+/** Optional value */
 class Option<const T> {
   constructor(private inner: T | null) {}
 
   static none = new Option<unknown>(null);
 
-  bind<U>(f: (inner: T) => Option<U>): Option<U> {
+  /** Returns `none` if the option is `none`, otherwise calls `f` with the wrapped value and returns the result. */
+  andThen<U>(f: (inner: T) => Option<U>): Option<U> {
     return this.inner === null ? (Option.none as Option<U>) : f(this.inner);
   }
 
@@ -38,7 +40,7 @@ class OptionComputation<const T> {
     f: (inner: T) => Option<A>
   ): OptionComputation<T & { [_ in Bound]: A }> {
     return new OptionComputation(
-      this.currentOpt.bind((bindings) =>
+      this.currentOpt.andThen((bindings) =>
         f(bindings).map((a) => {
           const newEntry = { [bound]: a } as { [_ in Bound]: A };
           return { ...bindings, ...newEntry };
@@ -52,7 +54,7 @@ class OptionComputation<const T> {
   }
 
   returnFrom<A>(f: (bindings: T) => Option<A>): Option<A> {
-    return this.currentOpt.bind(f);
+    return this.currentOpt.andThen(f);
   }
 }
 
